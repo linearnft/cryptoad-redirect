d4c443edfcfd4cb2e951489cdb01de16fd44140e
linearnft <linearnft@protonmail.com>
2021-09-10 21:18:08 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+let contact = document.getElementsByClassName("contact");
+console.log(contact)
+contact[0].onclick = () => {window.open("https://www.twitter.com");};
+contact[1].onclick = () => {window.open("https://www.discord.com");};
